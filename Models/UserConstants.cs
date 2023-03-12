d92ed66af7ef2547d73f4cc29a8cbb2983c96f19
Repodotnet <ssravya2710@gmail.com>
2023-03-12 20:56:28 +0530

    Add project files.

---
+++ Models/UserConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "john_admin", EmailAddress = "john.admin@email.com",
+                Password = "Password", GivenName = "John", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { Username = "mark_seller", EmailAddress = "Mark.seller@email.com",
+                Password = "Password", GivenName = "Mark", Surname = "Lambert", Role = "Seller" },
+        };
+    }
+}
